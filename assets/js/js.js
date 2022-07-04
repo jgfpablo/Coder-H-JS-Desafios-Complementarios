b75e4663fcb1e8ddf930189df6b53f819587c17c
Pablo Frank <jgfpablo@gmail.com>
2022-07-04 20:39:16 -0300

    segundo desafio

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,51 +1,160 @@
-//  Entrada Cine ------------------------------------------------
+let seleccionesDeUsuario = [];
+let numero = 0;
+let agregar = "Si";
+let precioTotal = 0;
 
-let edadPadre = prompt("escribe la edad del padre");
-let edadNiño = prompt("escribe la edad del niño");
+function menu(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+}
 
-let pagoDeEntrada = true;
+//FUNCION  SeleccionProducto ---------------------------------------------------------------------------------------------
+const seleccionProducto = (tiposMenu) => {
+    if (tiposMenu == 1) {
+        let Seleccion = prompt(
+            `seleccione una Bebida Caliente
+                \n 1-Macchiato Bombóm
+                \n 2-Capuccino Moka
+                \n 3-Cappuccino
+                \n 4-Cappuccino Tentación
+                \n Ingrese un numero para continuar`
+        );
 
-const saludar = () => {
-    console.log(saludo + " " + nombre);
+        switch (Seleccion) {
+            case "1":
+                seleccionesDeUsuario[numero] = new menu(
+                    "Macchiato Bombóm",
+                    255
+                );
+                break;
+            case "2":
+                seleccionesDeUsuario[numero] = new menu("Capuccino Moka", 255);
+                break;
+            case "3":
+                seleccionesDeUsuario[numero] = new menu("Cappuccino", 255);
+                break;
+            case "4":
+                seleccionesDeUsuario[numero] = new menu(
+                    "Cappuccino Tentación",
+                    255
+                );
+                break;
+        }
+    } else if (tiposMenu == 2) {
+        let Seleccion = prompt(
+            `seleccione una Bebida Fria
+                \n 1-Té de Frambuesa
+                \n 2-Té de Limon
+                \n 3-Té de Naranja
+                \n Ingrese un numero para continuar`
+        );
+
+        switch (Seleccion) {
+            case "1":
+                seleccionesDeUsuario[numero] = new menu("Té de Frambuesa", 255);
+                break;
+            case "2":
+                seleccionesDeUsuario[numero] = new menu("Té de Limon", 255);
+                break;
+            case "3":
+                seleccionesDeUsuario[numero] = new menu("Té de Naranja", 255);
+                break;
+        }
+    } else if (tiposMenu == 3) {
+        let Seleccion = prompt(
+            `seleccione un Salados
+                \n 1-Tostado 4 Quesos
+                \n 2-Tostado Mixto
+                \n 3-Tostado Napolitano
+                \n Ingrese un numero para continuar`
+        );
+
+        switch (Seleccion) {
+            case "1":
+                seleccionesDeUsuario[numero] = new menu(
+                    "Tostado 4 Quesos",
+                    255
+                );
+                break;
+            case "2":
+                seleccionesDeUsuario[numero] = new menu("Tostado Mixto", 255);
+                break;
+            case "3":
+                seleccionesDeUsuario[numero] = new menu(
+                    "Tostado Napolitano",
+                    255
+                );
+                break;
+        }
+    } else if (tiposMenu == 4) {
+        let Seleccion = prompt(
+            `seleccione un Postres
+                \n 1-Flan
+                \n 2-Pay de Queso
+                \n Ingrese un numero para continuar`
+        );
+
+        switch (Seleccion) {
+            case "1":
+                seleccionesDeUsuario[numero] = new menu("Flan", 255);
+                break;
+            case "2":
+                seleccionesDeUsuario[numero] = new menu("Pay de Queso", 255);
+                break;
+        }
+    } else {
+        alert("Porfavor Ingrese un numero de menu valido");
+        numero--;
+    }
 };
+// FIN FUNCION  SeleccionProducto ---------------------------------------------------------------------------------------------
 
-if (pagoDeEntrada && (edadNiño >= 18 || edadPadre >= 18)) {
-    console.log("Pueden entrar");
+//FUNCION repetirSeleccion -------------------------------------------------------------------------------------------------
+const repetirSeleccion = () => {
+    agregar = "";
+    console.table(seleccionesDeUsuario);
+    do {
+        agregar = prompt(
+            "si desea agregar algo a su menu escriba Si en caso contrario escriba No"
+        );
+    } while (
+        agregar != "Si" &&
+        agregar != "si" &&
+        agregar != "SI" &&
+        agregar != "No" &&
+        agregar != "no" &&
+        agregar != "NO"
+    );
+
+    if (agregar == "si" || agregar == "Si" || agregar == "SI") {
+        numero++;
+    }
+};
+//FIN FUNCION repetirSeleccion -------------------------------------------------------------------------------------------------
+
+while (agregar == "Si" || agregar == "si" || agregar == "SI") {
+    let tiposMenu = prompt(
+        `seleccione uno de nuestro menu de  productos
+        \n 1-Bebidas Calientes
+        \n 2-Bebidas Frias
+        \n 3-Salados
+        \n 4-Postres
+        \n Ingrese un numero para continuar`
+    );
+    seleccionProducto(tiposMenu);
+    repetirSeleccion();
+}
+if (agregar == "No" || agregar == "no" || agregar == "NO") {
+    console.log("\n");
+    console.log("Su Pedido es :");
+    console.table(seleccionesDeUsuario);
+
+    seleccionesDeUsuario.forEach((seleccionDeUsuario) => {
+        precioTotal += seleccionDeUsuario.precio;
+    });
+    console.log("El total de su compra es : " + precioTotal);
 } else {
-    console.log("No pueden entrar");
+    console.log(
+        "el programador no tiene idea de como llegaste a esta opcion reportalo para que asi este pueda recapacitar si seguir programando o dejarlo para siempre"
+    );
 }
-
-//   FIN ENTRADA CINE ---------------------------------------------
-
-//   TABLA DE UN NUMERO INGRESADO ------------------------------------------------
-let numeroIngresado = parseInt(
-    prompt("ingrese un numero para obtener la tabla de multiplicar del mismo")
-);
-let resultado = [];
-
-for (let index = 0; index <= 10; index++) {
-    resultado[index] = numeroIngresado * index;
-}
-console.table(resultado);
-
-//  FIN TABLA DE UN NUMERO INGRESADO ------------------------------------------------
-
-//  FIN FIBONACCI ---------------------------------------------
-
-let num = 0;
-let num2 = 1;
-let aux;
-let index = 0;
-let tabla = [];
-
-while (index < 100) {
-    index = index + 1;
-    aux = num + num2;
-    num = num2;
-    num2 = aux;
-    tabla[index] = aux;
-}
-
-console.table(tabla);
-
-// FIN FIBONACCI ---------------------------------------------
