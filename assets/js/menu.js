3a1cfaa373eccec63b0dae278b069fcb09060b09
Pablo Frank <jgfpablo@gmail.com>
2022-08-02 20:52:12 -0300

    Optimizando el proyecto final uso de operadores avanzados

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,3 @@
-let productos = [];
 let precioTotal = 0;
 class Producto {
     constructor(nombre, precio, cantidad) {
@@ -7,104 +6,139 @@ class Producto {
             (this.cantidad = cantidad);
     }
 
-    agregar() {
+    Agregar() {
         this.cantidad = this.cantidad + 1;
     }
-    quitar() {
+
+    Quitar() {
         if (this.cantidad >= 1) {
             this.cantidad = this.cantidad - 1;
         }
     }
 }
 
-total = () => {
-    precioTotal = 0;
+let productos = [
+    new Producto("Macchiato Bombom", 240, 0),
+    new Producto("Cappuccino Moka", 240, 0),
+    new Producto("Cappuccino", 220, 0),
+    new Producto("Cappuccino Tentación", 250, 0),
+    new Producto("Cappuccino Bombóm", 230, 0),
+    new Producto("Macchiato", 220, 0),
+    new Producto("Té de Frambuesa", 150, 0),
+    new Producto("Té de Limon", 120, 0),
+    new Producto("Té de Naranja", 140, 0),
+    new Producto("Tostado Napolitano", 200, 0),
+    new Producto("Tostado Mixto", 250, 0),
+    new Producto("Tostado 4 Quesos", 230, 0),
+    new Producto("Flan", 170, 0),
+    new Producto("Pay de Queso", 250, 0),
+];
+
+CargarLocalStorage = () => {
+    if (localStorage.productos) {
+        listaProductos = JSON.parse(localStorage.getItem("productos"));
+        for (const iterator of listaProductos) {
+            document.getElementById(iterator.nombre).value = iterator.cantidad;
+            productos.push(
+                new Producto(
+                    iterator.nombre,
+                    iterator.precio,
+                    iterator.cantidad
+                )
+            );
+        }
+    }
+};
+
+Total = () => {
+    for (const producto of productos) {
+        tdProducto = document.getElementById(`td-${producto.nombre}`);
+        trProducto = document.getElementById(`tr-${producto.nombre}`);
+        if (tdProducto) {
+            if (producto.cantidad > 0) {
+                tdProducto.innerHTML = producto.cantidad;
+            } else {
+                trProducto.remove();
+            }
+        } else {
+            if (producto.cantidad != 0) {
+                tr = document.createElement("tr");
+                tr.setAttribute("id", `tr-${producto.nombre}`);
+                td = document.createElement("td");
+                nombre = document.createTextNode(`${producto.nombre}`);
+                tabla.appendChild(tr).appendChild(td).appendChild(nombre);
+
+                td = document.createElement("td");
+                td.setAttribute("id", `td-${producto.nombre}`);
+                cantidad = document.createTextNode(`${producto.cantidad}`);
+                tabla.appendChild(tr).appendChild(td).appendChild(cantidad);
+            }
+        }
+    }
+
     let total = document.getElementById("total");
+    precioTotal = 0;
     for (const iterator of productos) {
         precioTotal = precioTotal + iterator.precio * iterator.cantidad;
     }
     total.innerHTML = precioTotal;
 };
 
-sumar = (id, precio) => {
-    if (id.includes("-")) {
-        nombre = id.replace(/-/g, " ");
-    } else {
-        nombre = id;
-    }
-    precio = Number(precio);
-    cantidad = document.getElementById(id);
-    cantidad.value++;
+Sumar = (nombre) => {
+    let posicion;
+    input = document.getElementById(nombre);
 
-    let verificacionExistenciaEnArray = productos.filter(
-        (item) => item.nombre == nombre
-    );
-    if (verificacionExistenciaEnArray.length != 0) {
-        verificacionExistenciaEnArray[0].agregar();
-    } else {
-        productos.push(
-            new Producto(nombre, precio, (cantidad = Number(cantidad.value)))
-        );
-    }
-    let tabla = document.getElementById("tabla");
-
-    for (const iterator of productos) {
-        tdProducto = document.getElementById(iterator.nombre);
-        if (tdProducto) {
-            aumentarCantidad = document.getElementById(
-                iterator.nombre + "cantidad"
-            );
-            aumentarCantidad.innerHTML = iterator.cantidad;
-        } else {
-            if (iterator.cantidad > 0) {
-                const tr = document.createElement("tr");
-                tr.setAttribute("id", iterator.nombre);
-
-                nombre = document.createTextNode(iterator.nombre);
-                cantidad = document.createTextNode(iterator.cantidad);
-
-                td = document.createElement("td");
-                tabla.appendChild(tr).appendChild(td).appendChild(nombre);
-                td = document.createElement("td");
-                td.setAttribute("id", iterator.nombre + "cantidad");
-                tabla.appendChild(tr).appendChild(td).appendChild(cantidad);
-            }
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].nombre == nombre) {
+            posicion = [i];
         }
+    }
+
+    productos[posicion].Agregar();
+    input.value = `${productos[posicion].cantidad}`;
+
+    localStorage.setItem("productos", JSON.stringify(productos));
+};
+
+Restar = (nombre) => {
+    let posicion;
+    input = document.getElementById(nombre);
+
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].nombre == nombre) {
+            posicion = [i];
+        }
+    }
+
+    if (productos[posicion].cantidad > 0) {
+        productos[posicion].Quitar();
+        input.value = `${productos[posicion].cantidad}`;
+    }
+
+    localStorage.setItem("productos", JSON.stringify(productos));
+};
+
+RealizarPedido = () => {
+    total = Number(document.getElementById("total").innerHTML);
+    if (total > 0) {
+        Swal.fire({
+            title: "Su pedido esta en proceso",
+            confirmButtonText: "OK",
+            icon: "success",
+            allowOutsideClick: false,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                localStorage.clear();
+                window.location.reload();
+            }
+        });
+    } else {
+        Swal.fire({
+            title: "Usted no posee productos en su carrito",
+            icon: "warning",
+            allowOutsideClick: false,
+        });
     }
 };
 
-restar = (id, precio) => {
-    console.log(productos);
-    precio = Number(precio);
-    nombre = id.replace("-", " ");
-    cantidad = document.getElementById(id);
-
-    if (cantidad.value <= 0) {
-    } else {
-        cantidad.value--;
-    }
-
-    let verificacionExistenciaEnArray = productos.filter(
-        (item) => item.nombre == nombre
-    );
-
-    if (verificacionExistenciaEnArray.length != 0) {
-        verificacionExistenciaEnArray[0].quitar();
-    } else {
-        productos.push(new Producto(nombre, precio, 0));
-    }
-
-    for (const iterator of productos) {
-        tdProducto = document.getElementById(iterator.nombre);
-        if (tdProducto) {
-            aumentarCantidad = document.getElementById(
-                iterator.nombre + "cantidad"
-            );
-            aumentarCantidad.innerHTML = iterator.cantidad;
-
-            if (iterator.cantidad == 0) {
-                tdProducto.remove();
-            }
-        }
-    }
-};
+CargarLocalStorage();
--- assets/js/resena.js
+++ assets/js/resena.js
@@ -1,45 +1,218 @@
-// -------------------------------
-enviar = () => {
-    nombre = document.getElementById("nombre");
-    correo = document.getElementById("correo");
-    texto = document.getElementById("texto");
-    const comentarios = document.getElementById("comentarios");
-    const article = document.createElement("article");
-    article.className = "col-12 col-md-4 col-lg-3 bg m-4 resenas";
+class Comentarios {
+    constructor(nombre, comentario, correo, img) {
+        (this.nombre = nombre),
+            (this.comentario = comentario),
+            (this.correo = correo),
+            (this.image = img);
+    }
+}
 
+let comentariosAbstraidos = [
+    new Comentarios(
+        "Linus Torvalds",
+        "El olor a café de calidad ya te hace entrar, tiene mesas adentro y una terraza acogedora, ideal si vas de charla con amigos o solo con tu laptop . Muy buena atención.",
+        "linus@gmail.com",
+        "../assets/img/personas/persona.jpg"
+    ),
+    new Comentarios(
+        "Jose Luis",
+        "El lugar es acogedor y el café de lo mejor que puedes encontrar.",
+        "Jose@gmail.com",
+        "../assets/img/personas/persona-1.jpg"
+    ),
+    new Comentarios(
+        "Maria Gomez",
+        "Este es un verdadero lugar para tomar un gran cafe.",
+        "Maria@gmail.com",
+        "../assets/img/personas/persona-2.jpg"
+    ),
+    new Comentarios(
+        "Mario Gonzalez",
+        "Excelente la comida, porciones grandes y muy buenas.",
+        "Mario@gmail.com",
+        "../assets/img/personas/persona-3.jpg"
+    ),
+    new Comentarios(
+        "Carlos Rivera",
+        "Fui a tomar un cafe y comer algunas cositas. El lugar está bueno, pero demoran demasiado.",
+        "Carlos@gmail.com",
+        "../assets/img/personas/persona-4.jpg"
+    ),
+    new Comentarios(
+        "Fernando Ruiz",
+        "Excelente ambiente, atención y bebidas. Un lugar mágico para apreciadores de café. Además de apreciar distintos tipos de café, se puede conocer más acerca de las variaciones. Recomendo.",
+        "Fernando@gmail.com",
+        "../assets/img/personas/persona-5.jpg"
+    ),
+];
+
+carga = (index) => {
+    // desestructuración de OBJ
+    let { nombre, comentario, image } = comentariosAbstraidos[index];
+    article = document.createElement("article");
+    article.className = "col-12 col-md-4 col-lg-3 bg m-4 resenas";
     div = document.createElement("div");
     div.className = "d-flex justify-content-center pt-4";
     img = document.createElement("img");
-    article
-        .appendChild(div)
-        .appendChild(img)
-        .setAttribute("src", "../assets/img/quote-img.png");
+    img.setAttribute("src", "../assets/img/quote-img.png");
+    article.appendChild(div).appendChild(img);
 
-    // -------------------------------------
     div = document.createElement("div");
-    div.className = "d-flex justify-content-center pt-4";
+    div.className = "d-flex justify-content-center mt-4 text-card";
     span = document.createElement("span");
-    span.className = "text-center";
-    span.textContent = texto.value;
-    article.appendChild(div).appendChild(span);
-
-    // -------------------------------
+    span.className = "text-center span-text";
+    span.setAttribute("id", `${index}`);
+    text = document.createTextNode(comentario);
+    comentarios
+        .appendChild(article)
+        .appendChild(div)
+        .appendChild(span)
+        .appendChild(text);
 
     div = document.createElement("div");
     div.className = "d-flex justify-content-center mt-4";
     img = document.createElement("img");
+    console.log(comentariosAbstraidos);
+    img.setAttribute("src", image);
     img.className = "border-100 imagen-resena";
-    img.setAttribute("src", "../assets/img/personas/persona.jpg");
-    article.appendChild(div).appendChild(img);
-
-    // -------------------------------
+    comentarios.appendChild(article).appendChild(div).appendChild(img);
 
     div = document.createElement("div");
-    div.className = " d-flex justify-content-center fs-2 text-center";
+    div.className = "d-flex justify-content-center fs-2 text-center";
     span = document.createElement("span");
     span.className = "color fs-1";
-    span.textContent = nombre.value;
-    article.appendChild(div).appendChild(span);
+    text = nombre;
+    comentarios
+        .appendChild(article)
+        .appendChild(div)
+        .appendChild(span).textContent = text;
 
-    comentarios.appendChild(article);
+    agregarSpanText(index);
+    verMenos(index);
 };
+
+AgregarNuevoComentario = () => {
+    nombre = document.getElementById("nombre").value;
+    correo = document.getElementById("correo").value;
+    texto = document.getElementById("texto").value;
+    img = "../assets/img/personas/persona.jpg";
+
+    if (nombre != "" && texto != "" && correo.includes("@")) {
+        comentariosAbstraidos.push(new Comentarios(nombre, texto, correo, img));
+        localStorage.setItem(
+            "comentarios",
+            JSON.stringify(comentariosAbstraidos)
+        );
+        Swal.fire({
+            title: "su comentario ha sido publicado con exito",
+            confirmButtonText: "OK",
+            icon: "success",
+            allowOutsideClick: false,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                carga(comentariosAbstraidos.length - 1);
+                localStorage.formulario = "";
+                window.location.reload();
+            }
+        });
+    } else {
+        Swal.fire({
+            title: "Ingrese correctamente los campos del formulario",
+            confirmButtonText: "OK",
+            icon: "error",
+            allowOutsideClick: false,
+        });
+    }
+};
+
+CargarTodosLosComentarios = () => {
+    for (let index = 0; index < comentariosAbstraidos.length; index++) {
+        carga(index);
+    }
+};
+
+agregarSpanText = (index) => {
+    span = document.getElementById(index);
+    span.setAttribute("onmouseover", `verMas(${index})`);
+    span.setAttribute("onmouseout", `verMenos(${index})`);
+};
+
+verMas = (id) => {
+    texto = document.getElementById(id).textContent =
+        comentariosAbstraidos[id].comentario;
+};
+
+verMenos = (id) => {
+    let recorte = comentariosAbstraidos[id].comentario.substring(0, 64);
+    if (comentariosAbstraidos[id].comentario.length > 65) {
+        recorte = recorte + "...";
+
+        span = document.getElementById(id);
+        span.textContent = recorte;
+
+        p = document.createElement("p");
+        p.className = "leerMas";
+        p.textContent = "▼";
+
+        span.appendChild(p);
+    } else {
+        comentario = comentariosAbstraidos[id].comentario;
+        document.getElementById(id).textContent = comentario;
+    }
+};
+
+if (localStorage.comentarios) {
+    comentariosAbstraidos = [];
+    coment = JSON.parse(localStorage.getItem("comentarios"));
+    console.log(coment);
+    for (let index = 0; index < coment.length; index++) {
+        comentariosAbstraidos[index] = new Comentarios(
+            coment[index].nombre,
+            coment[index].comentario,
+            coment[index].correo,
+            coment[index].image
+        );
+    }
+}
+
+//  ----------- OPERADOR LOGICO AND
+// if (localStorage.length == 0) {
+//     localStorage.setItem("comentarios", JSON.stringify(comentariosAbstraidos));
+// }
+localStorage.length == 0 &&
+    localStorage.setItem("comentarios", JSON.stringify(comentariosAbstraidos));
+// ----------- FIN OPERADOR LOGICO AND
+
+localStorageFormulario = () => {
+    setTimeout(() => {
+        formulario = [];
+
+        nombre = document.getElementById("nombre").value;
+        correo = document.getElementById("correo").value;
+        texto = document.getElementById("texto").value;
+
+        // ------------------- OPERADOR SPREAD
+        formulario = [...formulario, nombre, correo, texto];
+        console.log("2" + formulario);
+        // formulario.push(nombre);
+        // formulario.push(correo);
+        // formulario.push(texto);
+        //---------------------  FIN OPERADOR SPREAD
+
+        localStorage.setItem("formulario", JSON.stringify(formulario));
+    }, 10);
+};
+
+cargarFormularioComentario = () => {
+    if (localStorage.formulario) {
+        formulario = JSON.parse(localStorage.getItem("formulario"));
+
+        document.getElementById("nombre").value = formulario[0];
+        document.getElementById("correo").value = formulario[1];
+        document.getElementById("texto").value = formulario[2];
+    }
+};
+
+CargarTodosLosComentarios();
+
+cargarFormularioComentario();
